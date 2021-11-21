6cf1e44f01c0e7e471b08ab5e6ece8b573c98ba0
adaletinsesi1515 <memduhgumen@gmail.com>
2021-11-21 20:22:45 +0300

    24 ders sonu

--- CoreDemo/Models/ErrorViewModel.cs
+++ CoreDemo/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace CoreDemo.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
