b0d7acba4116bdd3679bb8157420946fbe1d31aa
SÜLEYMAN YİĞİT GÜMEN <suleymanyigit.gumen@stu.bahcesehir.k12.tr>
2021-12-22 10:47:51 +0300

    70.ders  sonu

--- CoreDemo/Controllers/DashboardController.cs
+++ CoreDemo/Controllers/DashboardController.cs
@@ -1,13 +1,22 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class DashboardController : Controller
     {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
         [AllowAnonymous]
         public IActionResult Index()
         {
+            Context c = new Context();
+            ViewBag.v1 = c.Blogs.Count();
+            ViewBag.v2 = c.Blogs.Where(x=>x.WriterID==1).Count();
+            ViewBag.v3 = c.Categories.Count();
+
             return View();
         }
     }
