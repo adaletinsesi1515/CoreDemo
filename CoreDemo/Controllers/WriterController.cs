65633686ecaaeb95d19c1cd2e0d03fe63b0ee865
adaletinsesi1515 <memduhgumen@gmail.com>
2021-12-23 22:48:26 +0300

    72.ders sonu

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
@@ -6,6 +11,7 @@ namespace CoreDemo.Controllers
     [AllowAnonymous]
     public class WriterController : Controller
     {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
         public IActionResult Index()
         {
             return View();
@@ -21,6 +27,32 @@ namespace CoreDemo.Controllers
             return PartialView();
         }
 
+        [HttpGet]
+        public IActionResult WriterEditProfile()
+        {
+            var values = wm.TGetById(1);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer p)
+        {
+            WriterValidator wl = new WriterValidator();
+            ValidationResult result = wl.Validate(p);
+            if (result.IsValid)
+            {
+                p.WriterStatus = true;
+                wm.TUpdate(p);
+                return RedirectToAction("Index","Dashboard");
+            }
+            else
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
 
     }
 }
