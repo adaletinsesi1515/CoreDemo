040e69cf1ceb2b62636ce36caa892edf59574396
adaletinsesi1515 <memduhgumen@gmail.com>
2021-12-23 23:40:46 +0300

    Writer Image işlemi yapıldı

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
+using CoreDemo.Models;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
@@ -34,15 +35,31 @@ namespace CoreDemo.Controllers
             return View(values);
         }
         [HttpPost]
-        public IActionResult WriterEditProfile(Writer p)
-        {
+        public IActionResult WriterEditProfile(AddProfileImage p, Writer x)
+        {            
             WriterValidator wl = new WriterValidator();
-            ValidationResult result = wl.Validate(p);
+            ValidationResult result = wl.Validate(x);
             if (result.IsValid)
             {
-                p.WriterStatus = true;
-                wm.TUpdate(p);
-                return RedirectToAction("Index","Dashboard");
+                if (p.WriterImage != null)
+                {
+                    var extension = Path.GetExtension(p.WriterImage.FileName);
+                    var newImageName = Guid.NewGuid() + extension;
+                    var location = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/WriterImageFiles/",
+                        newImageName);
+                    var stream = new FileStream(location, FileMode.Create);
+                    p.WriterImage.CopyTo(stream);
+                    x.WriterImage = newImageName;
+                }                
+
+                x.WriterName = p.WriterName;
+                x.WriterAbout = p.WriterAbout;
+                x.WriterMail = p.WriterMail;
+                x.WriterPassword = p.WriterPassword;
+                x.WriterPassword2 = p.WriterPassword2;
+                x.WriterStatus = true;
+                wm.TUpdate(x);
+                return RedirectToAction("Index", "Dashboard");
             }
             else
             {
