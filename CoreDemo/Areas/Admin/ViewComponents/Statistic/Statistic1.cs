f73baf28cfc67091a8bed103441716432b9660ed
adaletinsesi1515 <memduhgumen@gmail.com>
2022-01-09 20:26:02 +0300

    99.ders sonu

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -0,0 +1,21 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic1 : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        ContactManager cm = new ContactManager(new EfContactRepository());
+        CommentManager ccm = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = bm.ListAll().Count();
+            ViewBag.v2 = cm.ListAll().Count();
+            ViewBag.v3 = ccm.ListAll().Count();
+            return View();
+        }
+
+    }
+}
