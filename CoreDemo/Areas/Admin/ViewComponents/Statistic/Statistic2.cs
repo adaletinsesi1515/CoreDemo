a9681a21dc5bd40c72bf55be59074dbfa8840feb
adaletinsesi1515 <memduhgumen@gmail.com>
2022-01-10 21:18:45 +0300

    100.ders sonu

---
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic2.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
+{
+    public class Statistic2 : ViewComponent
+    {
+        Context c = new Context();
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.v1 = c.Blogs.OrderByDescending(x=>x.BlogID).Select(x=>x.BlogTitle).Take(1).FirstOrDefault();
+            //ViewBag.v2 = cm.ListAll().Count();
+            //ViewBag.v3 = ccm.ListAll().Count();
+            return View();
+        }
+    }
+}
