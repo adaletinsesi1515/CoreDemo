afe1c6c40682ff292f3c0de4364642f2557689d1
SÜLEYMAN YİĞİT GÜMEN <suleymanyigit.gumen@stu.bahcesehir.k12.tr>
2022-01-11 16:50:34 +0300

    102.DERS SONU

---
+++ BusinessLayer/Concrete/AdminManager.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class AdminManager : IAdminService
+    {
+        IAdminDal _admindal;
+
+        public AdminManager(IAdminDal admindal)
+        {
+            _admindal = admindal;
+        }
+
+        public List<Admin> ListAll()
+        {
+            return _admindal.GetListAll();
+        }
+
+        public List<Admin> ListAllParameter(int id)
+        {
+            return _admindal.GetListAll(x=>x.AdminID == id);
+        }
+
+        public void TAdd(Admin t)
+        {
+            _admindal.Insert(t);
+        }
+
+        public Admin TGetById(int id)
+        {
+            return _admindal.GetById(id);
+        }
+
+        public void TRemove(Admin t)
+        {
+            _admindal.Delete(t);
+        }
+
+        public void TUpdate(Admin t)
+        {
+            _admindal.Update(t);
+        }
+    }
+}
