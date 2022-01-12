bd2c8a7be746df06f859f488af8f5db379f6b1f7
SÜLEYMAN YİĞİT GÜMEN <suleymanyigit.gumen@stu.bahcesehir.k12.tr>
2022-01-12 14:59:15 +0300

    Hava durumu çektik

--- CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreDemo/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
 {
@@ -14,6 +15,17 @@ namespace CoreDemo.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.ListAll().Count();
             ViewBag.v2 = cm.ListAll().Count();
             ViewBag.v3 = ccm.ListAll().Count();
+
+            //Hava durumunu API üzerinden çekme işlemi (Türkçe dil desteği var)
+            string api_key = "b3de9d0a7f8f915aec4b674625194dcc";
+            string connection = "http://api.openweathermap.org/data/2.5/weather?q=Burdur&mode=xml&units=metric&lang=tr&appid=" + api_key;
+            XDocument document = XDocument.Load(connection);
+            //Derece bilgisi
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+            //Bulut - Güneş bilgisi
+            ViewBag.v5 = document.Descendants("clouds").ElementAt(0).Attribute("name").Value;
+
+
             return View();
         }
 
