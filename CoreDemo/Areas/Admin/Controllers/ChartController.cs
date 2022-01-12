6b515c34e3552025ba90a04c2ef1ed9a80f0b10b
SÜLEYMAN YİĞİT GÜMEN <suleymanyigit.gumen@stu.bahcesehir.k12.tr>
2022-01-12 17:14:40 +0300

    Google Chart geçildi

---
+++ CoreDemo/Areas/Admin/Controllers/ChartController.cs
@@ -0,0 +1,24 @@
+﻿using CoreDemo.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CategoryChart()
+        {
+            List<CategoryClass> list = new List<CategoryClass>();
+            list.Add(new CategoryClass { CategoryName="Teknoloji", CategoryCount=10});
+            list.Add(new CategoryClass { CategoryName = "Yazılım", CategoryCount = 14 });
+            list.Add(new CategoryClass { CategoryName = "Spor", CategoryCount = 5 });
+            list.Add(new CategoryClass { CategoryName = "Sinema", CategoryCount = 2 });
+            return Json(new {Jsonlist = list});
+        }
+    }
+}
