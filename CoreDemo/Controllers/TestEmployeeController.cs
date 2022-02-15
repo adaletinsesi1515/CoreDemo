dd16393827c06f9843bb976caa8898f4393721eb
adaletinsesi1515 <memduhgumen@gmail.com>
2022-02-15 21:20:31 +0300

    APİ 1

---
+++ CoreDemo/Controllers/TestEmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace CoreDemo.Controllers
+{
+    public class TestEmployeeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:44319/api/Default");
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var value = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+            return View(value);
+        }
+
+        public class Class1
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+
+
+
+
+    }
+}
